57c9e679832ec9f0f7da4085d48524287c37f6cb
Loskutov Maxim <maksim.loskutov@murka.om>
2018-05-18 13:28:03 +0300

    [+] add sckipt unit base

---
+++ Chmario/Assets/Scripts/BaseUnit.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BaseUnit : MonoBehaviour
+{
+    [SerializeField]
+    protected int hp = 5;
+    [SerializeField]
+    protected float moveSpid = 1f;
+
+    protected void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    protected void Move()
+    {
+        float translation = Input.GetAxis("Horizontal") * moveSpid * Time.deltaTime;
+        transform.Translate(translation, 0, 0);
+    }
+
+    void Update ()
+    {
+        Move();
+
+        if(transform.position.y < -1)
+        {
+            Die();
+            SceneManager.LoadScene(0);
+        }		
+	}
+}
